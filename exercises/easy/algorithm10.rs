55c49cd7542f22e82376cbaab605f05842321b38
MrTwoc <1191422391@qq.com>
2025-02-10 21:04:50 +0800

    easy10

--- exercises/easy/algorithm10.rs
+++ exercises/easy/algorithm10.rs
@@ -28,20 +28,78 @@ impl Graph for UndirectedGraph {
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>> {
         &self.adjacency_table
     }
+    /// 添加一条边到图中
+    /// 
+    /// 如果边的两个节点都不存在，则先添加节点，然后添加边。
+    /// 
+    /// # 参数
+    /// 
+    /// * `edge` - 一个包含边的两个节点和权重的元组
+    /// 
+    /// # 示例
+    /// 
+    /// ```
+    /// let mut graph = UndirectedGraph::new();
+    /// graph.add_edge(("a", "b", 5));
+    /// ```
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
         //TODO
+        // 解构边的元组，获取起始节点、目标节点和权重
+        let (from_node, to_node, weight) = edge;
+        // 检查起始节点是否存在，如果不存在，则添加节点
+        if !self.contains(from_node) {
+            self.add_node(from_node);
+        }
+        // 检查目标节点是否存在，如果不存在，则添加节点
+        if !self.contains(to_node) {
+            self.add_node(to_node);
+        }
+        // 获取起始节点的邻接表，并添加目标节点和权重
+        self.adjacency_table_mutable().get_mut(from_node).unwrap().push((to_node.to_string(), weight));
+        // 获取目标节点的邻接表，并添加起始节点和权重
+        self.adjacency_table_mutable().get_mut(to_node).unwrap().push((from_node.to_string(), weight));
     }
 }
 pub trait Graph {
     fn new() -> Self;
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
+    /// 添加一个节点到图中
+    /// 
+    /// 如果节点已经存在，则返回false；否则，添加节点并返回true。
+    /// 
+    /// # 参数
+    /// 
+    /// * `node` - 要添加的节点的名称
+    /// 
+    /// # 返回值
+    /// 
+    /// 如果节点已经存在，则返回false；否则，返回true。
     fn add_node(&mut self, node: &str) -> bool {
         //TODO
-		true
+		// true
+        // 检查节点是否已经存在于邻接表中
+        if self.adjacency_table().contains_key(node) {
+            // 如果节点已经存在，返回false
+            false
+        } else {
+            // 如果节点不存在，将其添加到邻接表中，并返回true
+            self.adjacency_table_mutable().insert(node.to_string(),Vec::new());
+            true
+        }
+
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
         //TODO
+        let (from_node, to_node, weight) = edge;
+        if !self.contains(from_node){
+            self.add_node(from_node);
+        }
+        if !self.contains(to_node){
+            self.add_node(to_node);
+        }
+        self.adjacency_table_mutable().get_mut(from_node).unwrap().push((to_node.to_string(), weight));
+        self.adjacency_table_mutable().get_mut(to_node).unwrap().push((from_node.to_string(), weight));
     }
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
