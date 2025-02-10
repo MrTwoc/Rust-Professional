b93466cc6da0846c205906bf5ab84a443c17d976
MrTwoc <1191422391@qq.com>
2025-02-10 18:14:46 +0800

    easy9

--- exercises/easy/algorithm9.rs
+++ exercises/easy/algorithm9.rs
@@ -35,9 +35,28 @@ where
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
-
+    /// 将元素添加到堆中
     pub fn add(&mut self, value: T) {
-        //TODO
+        // 将元素添加到堆的末尾
+        self.items.push(value);
+        // 增加堆的大小
+        self.count += 1;
+        // 从最后一个元素开始向上调整堆
+        self.switch(self.count);
+    }
+
+    /// 从指定索引开始向上调整堆
+    fn switch(&mut self, idx: usize) {
+        let mut idx = idx;
+        // 当当前节点不是根节点且当前节点的值小于其父节点的值时
+        while idx > 1 && (self.comparator)(&self.items[idx], &self.items[self.parent_idx(idx)]) {
+            // 获取父节点的索引
+            let parent_index = self.parent_idx(idx);
+            // 交换当前节点和其父节点的位置
+            self.items.swap(idx, parent_index);
+            // 更新当前节点的索引为其父节点的索引
+            idx = self.parent_idx(idx);
+        }
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -55,10 +74,54 @@ where
     fn right_child_idx(&self, idx: usize) -> usize {
         self.left_child_idx(idx) + 1
     }
-
+    /// 获取当前节点的最小子节点的索引
     fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-		0
+        // 获取当前节点的左子节点索引
+        let left = self.left_child_idx(idx);
+        // 获取当前节点的右子节点索引
+        let right = self.right_child_idx(idx);
+
+        // 如果右子节点存在且右子节点的值小于左子节点的值，则返回右子节点的索引
+        if right <= self.count && (self.comparator)(&self.items[right], &self.items[left]) {
+            right
+        // 否则返回左子节点的索引
+        } else {
+            left
+        }
+    }
+
+    /// 从指定索引开始向下调整堆
+    fn re_switch(&mut self, idx: usize) {
+        let mut idx = idx;
+        // 当当前节点存在子节点时
+        while self.children_present(idx) {
+            // 获取当前节点的最小子节点的索引
+            let smallest_child = self.smallest_child_idx(idx);
+            // 如果最小子节点的值小于当前节点的值，则交换它们的位置
+            if (self.comparator)(&self.items[smallest_child], &self.items[idx]) {
+                self.items.swap(idx, smallest_child);
+                idx = smallest_child;
+            // 否则跳出循环
+            } else {
+                break;
+            }
+        }
+    }
+
+    /// 移除并返回堆顶元素
+    pub fn pop(&mut self) -> Option<T> {
+        // 如果堆为空，则返回None
+        if self.is_empty() {
+            return None;
+        }
+        // 将堆顶元素与最后一个元素交换位置
+        self.items.swap(1, self.count);
+        // 减少堆的大小
+        self.count -= 1;
+        // 从堆顶开始向下调整堆
+        self.re_switch(1);
+        // 移除并返回最后一个元素
+        self.items.pop()
     }
 }
 
@@ -84,8 +147,7 @@ where
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
-        //TODO
-		None
+        self.pop()
     }
 }
 
